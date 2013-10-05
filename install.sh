3cea2af5d32b36f58c1dd598ee27f60975965275
tony baldwin <tony@tonybaldwin.org>
2013-10-05 08:57:48 -0400

    minor change

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # If you are using Windows, I can't help you.
 ##########################################################3
 
-if [ != "$HOME/bin/" ]; then
+if [ ! -d "$HOME/bin/" ]; then
 	mkdir $HOME/bin/
 	$PATH=$PATH:/$HOME/bin/
 	export PATH
@@ -15,6 +15,7 @@ fi
 cp tkount.tcl $HOME/bin/tkount
 chmod +x $HOME/bin/tkount
 
-echo -e "Installation complete!\n
-To run tkount, execute \"tkount\" from terminal, or make a shortcut or menu item that points to $HOME/bin/tkount."
+echo -e "Installation complete!
+To run tkount, execute \"tkount\" from terminal, 
+or make a shortcut or menu item that points to $HOME/bin/tkount."
 
